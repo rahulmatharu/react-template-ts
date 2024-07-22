50e7d944d58ccf5edc4fd65578770de898075e6a
rahul-matharu <116460268+rahul-matharu@users.noreply.github.com>
2024-07-22 20:50:44 +0100

    showing error on ui when api call fails

--- src/components/ExpensesTable.tsx
+++ src/components/ExpensesTable.tsx
@@ -13,8 +13,11 @@ type Expense = {
 
 const ExpensesTable = () => {
   const [expenses, setExpenses] = useState<Expense[]>([]);
+  const [error, setError] = useState<string>();
 
   useEffect(() => {
+    //TODO: Add loading spinner while data is being fetched
+
     fetch("https://expenses-backend-mu.vercel.app/expenses", {
       headers: {
         "Content-Type": "application/json",
@@ -27,36 +30,50 @@ const ExpensesTable = () => {
         setExpenses(data);
       })
       .catch((error) => {
+        setError("Unable to load expenses. Please try again later.");
         console.error("Error fetching expenses: ", error);
       });
   }, []);
 
+  //TODO: Add functionality to sort columns
+  /* 
+  1. Add an onclick onto each table heading to make it clickable
+  2. Capture useState of which column has been sorted and whether it is asc or desc
+  3. Display arrow icon on the sorted column with its orientation dependent on whether its asc or desc
+  4. Create a function to sort the json data returned by API
+  
+  */
+
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Merchant</th>
-            <th>Amount</th>
-            <th>Category</th>
-            <th>Description</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {expenses.map((expense: Expense) => (
-            <tr key={expense.id}>
-              <td>{formatDate(expense.date)}</td>
-              <td>{expense.merchant}</td>
-              <td>£{expense.amount}</td>
-              <td>{capitaliseFirstLetter(expense.category)}</td>
-              <td>{expense.description}</td>
-              <td>{capitaliseFirstLetter(expense.status)}</td>
+      {error ? (
+        <div>{error}</div>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Merchant</th>
+              <th>Amount</th>
+              <th>Category</th>
+              <th>Description</th>
+              <th>Status</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {expenses.map((expense: Expense) => (
+              <tr key={expense.id}>
+                <td>{formatDate(expense.date)}</td>
+                <td>{expense.merchant}</td>
+                <td>£{expense.amount}</td>
+                <td>{capitaliseFirstLetter(expense.category)}</td>
+                <td>{expense.description}</td>
+                <td>{capitaliseFirstLetter(expense.status)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
